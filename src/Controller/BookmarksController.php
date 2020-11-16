3b608d8229e7469dbcce8da7823ac2e337948304
Matthis Moyon <matthis.moyon@hotmail.com>
2020-11-16 10:43:46 +0100

    Generated Controllers

---
+++ src/Controller/BookmarksController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookmarksController extends AbstractController
+{
+    /**
+     * @Route("/bookmarks", name="bookmarks")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/BookmarksController.php',
+        ]);
+    }
+}
